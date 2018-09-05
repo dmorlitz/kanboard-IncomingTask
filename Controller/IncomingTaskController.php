00cbd7c640e9c142db1a099c33a024f7be32f953
David Morlitz <david@morlitz.com>
2018-09-05 08:13:58 -0700

    Added HTTP error codes

--- Controller/IncomingTaskController.php
+++ Controller/IncomingTaskController.php
@@ -23,8 +23,8 @@ class IncomingTaskController extends BaseController
         $this->checkWebhookToken();
 
 //Debug code
-$req_dump = print_r($_REQUEST, true);
-$fp = file_put_contents('/tmp/IncomingTask.log', $req_dump, FILE_APPEND);
+//$req_dump = print_r($_REQUEST, true);
+//$fp = file_put_contents('/tmp/IncomingTask.log', $req_dump, FILE_APPEND);
 //$req_dump = print_r($_POST, true);
 //$fp = file_put_contents('/tmp/IncomingTask.log', $req_dump, FILE_APPEND);
 
@@ -33,39 +33,50 @@ $fp = file_put_contents('/tmp/IncomingTask.log', $req_dump, FILE_APPEND);
         $incomingtask_column_id   = $this->configModel->get('incomingtask_column_id');
         $incomingtask_swimlane_id = $this->configModel->get('incomingtask_swimlane_id');
 
+        // Determine if we need to send HTTP status codes or descriptive text error messages
+        $send_http_error_codes = true;
+        if ( (isset($_REQUEST['response_url'])) && (strpos($_REQUEST['response_url'], "slack.com") !== false) ) { $send_http_error_codes = false; }
+
         if ($this->configModel->get('incomingtask_subject') == "") {
+           if ($send_http_error_codes) { http_response_code(500); }
            echo "ERROR: Subject field is not defined - please check your Kanboard configuration";
            exit(1);
         }
 
         if (intval($incomingtask_project_id) == 0) {
+           if ($send_http_error_codes) { http_response_code(500); }
            echo "ERROR: Project to insert task into is not defined - please check your Kanboard configuration";
            exit(1);
         }
 
         if (!in_array($incomingtask_project_id, $this->projectModel->getAllIds())) {
-          echo("ERROR: Project " . $incomingtask_project_id . " does not appear to exist - task insertion will FAIL");
-          exit(1);
+           if ($send_http_error_codes) { http_response_code(500); }
+           echo("ERROR: Project " . $incomingtask_project_id . " does not appear to exist - task insertion will FAIL");
+           exit(1);
         }
 
         if (intval($incomingtask_column_id) == 0) {
+           if ($send_http_error_codes) { http_response_code(500); }
            echo "ERROR: Column to insert task into is not defined - please check your Kanboard configuration";
            exit(1);
         }
 
         if ($incomingtask_project_id != $this->columnModel->getProjectId($incomingtask_column_id)) {
-          echo("ERROR: Column " . $incomingtask_column_id . " is not in project " . $incomingtask_project_id . " - task insertion will FAIL");
-          exit(1);
+           if ($send_http_error_codes) { http_response_code(500); }
+           echo("ERROR: Column " . $incomingtask_column_id . " is not in project " . $incomingtask_project_id . " - task insertion will FAIL");
+           exit(1);
         }
 
         if (intval($incomingtask_swimlane_id) == 0) {
+           if ($send_http_error_codes) { http_response_code(500); }
            echo "ERROR: Swimlane to insert task into is not defined - please check your Kanboard configuration";
            exit(1);
         }
 
         if (!array_key_exists($incomingtask_swimlane_id, $this->swimlaneModel->getList($incomingtask_project_id))) {
-          echo("ERROR: Swimlane " . $incomingtask_swimlane_id . " does not appear to exist in project " . $incomingtask_project_id . " - task insertion will FAIL");
-          exit(1);
+           if ($send_http_error_codes) { http_response_code(500); }
+           echo("ERROR: Swimlane " . $incomingtask_swimlane_id . " does not appear to exist in project " . $incomingtask_project_id . " - task insertion will FAIL");
+           exit(1);
         }
 
         //Try to find the first text field provided that has a value
@@ -80,13 +91,15 @@ $fp = file_put_contents('/tmp/IncomingTask.log', $req_dump, FILE_APPEND);
         }
 
         if ($found == false) {
-            echo "ERROR: You asked to look in the fields named " . implode(",", $subject_fields) . " but none of these were found in the data sent";
-            exit(1);
+           if ($send_http_error_codes) { http_response_code(500); }
+           echo "ERROR: You asked to look in the fields named " . implode(",", $subject_fields) . " but none of these were found in the data sent";
+           exit(1);
         }
 
         if ($subject == "") {
-          echo("ERROR: No text was sent for the task name - ABORT");
-          exit(1);
+           if ($send_http_error_codes) { http_response_code(500); }
+           echo("ERROR: No text was sent for the task name - ABORT");
+           exit(1);
         }
 
         $result = $this->taskCreationModel->create(array(
@@ -98,8 +111,10 @@ $fp = file_put_contents('/tmp/IncomingTask.log', $req_dump, FILE_APPEND);
                                                   );
 
         if ($result > 0) {
+           if ($send_http_error_codes) { http_response_code(500); }
            echo("Kanboard accepted a task titled '" . $subject . "' as task number " . $result);
         } else {
+           if ($send_http_error_codes) { http_response_code(500); }
            echo("Something went wrong and Kanboard did not accept your task");
         }
     }
